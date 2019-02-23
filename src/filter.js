b5940d976c024987fd2b163bc8b8c527bcad94d9
jiameng7878 <meng.jia@lvyuetravel.com>
2019-02-23 18:04:39 +0800

    增加filter

--- src/filter.js
+++ src/filter.js
@@ -1,28 +1,28 @@
 import _curry2 from './utils/_curry2';
 import _checkMethod from './utils/_checkMethod';
+import _isObject from './utils/_isObject';
+import _filter from './utils/_filter';
 import reduce from './reduce';
-import type from './type';
 import keys from './keys';
-
-var filter = _curry2(function(fn, list) {
-    var acc;
-    switch (type(list)) {
-        case 'Object':
-            acc = {};
-            break;
-        default:
-            acc = [];
-            break;
-    }
-
-    return reduce(function(acc, key) {
-        var result  = fn(list[key]);
-        if(result) {
-            acc[key] = list[key]
-        };
-       
-        return acc;
-    }, keys(list), acc);
-});
+/**
+ * 迭代列表，在每个函数上调用断言函数，返回满足断言函数的元素。返回值与传入的列表类型相同
+ * @func
+ * @member {List}
+ * @param {Function} fn 断言函数
+ * @parma {*} list 数组或者对象
+ * @returns {*} 根据传入的对象类型，返回一个新的数组或对象
+ * 
+ */
+var filter = _curry2(_checkMethod('filter', function (fn, list) {
+    return  _isObject(list) ? 
+            reduce(function(acc, key) {
+               var result  = fn(list[key]);
+               if(result) {
+                   acc[key] = list[key]
+               };
+               return acc;
+           }, keys(list), {}) : _filter(fn, list);
+       }
+));
 
 export default filter;
