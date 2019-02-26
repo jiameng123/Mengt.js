bc545d5bdfd445b583e9b243fb96ea7eca4f810e
jiameng7878 <meng.jia@lvyuetravel.com>
2019-02-26 19:07:12 +0800

    update

--- src/utils/_partial.js
+++ src/utils/_partial.js
@@ -1,7 +1,15 @@
-import _arity from "./_arity";
-
-
-export default function _partinal(fn, argArr) {
-
-    
+/**
+ * 
+ * @private
+ * @function
+ * @name _partial
+ * @todo 部分应用从左至右
+ * @param {function} fn 部分应用转换的函数
+ */
+export default function _partial(fn) {
+    var args = [].slice.call(arguments, 1);
+    return function g () {
+        var args2 = args.concat([].slice.call(arguments));
+        return  args2.length < fn.length ?  g : fn.apply(this, args2);
+    }
 }
