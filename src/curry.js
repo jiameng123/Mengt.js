f51b6dda64681ebc19df98ca74ddd840c4a0ba62
jiameng <386805508@qq.com>
2019-03-02 17:34:58 +0800

    增加柯里化占位符功能

--- src/curry.js
+++ src/curry.js
@@ -6,15 +6,11 @@ import _isFunction  from './utils/_isFunction';
  * 
  * 柯里化未知参数个数的函数
  * @func curry
- * 
  * @member {Function}
  * @returns {Function} 
  * 
  */
 var curry =  _curry1(function curry (fn) {
-    if(!_isFunction(fn)) {
-         throw new Error(fn+ ' is not a function!');
-    }
     return curryN(fn.length, fn);
 });
 
--- src/utils/_curryN.js
+++ src/utils/_curryN.js
@@ -1,27 +1,28 @@
-import _isFunction from './_isFunction';
-import _arity from './_arity';
-/**
- * 
- * @function
- * @name _curryN
- * @todo 多参数函数柯里化增加占位符
- * @private
- * @param {Function} 将fn转化成柯里化 
- */
+import _isPlaceholder from "./_isPlaceholder";
+import _arity from "./_arity";
 
-export default function _curryN(fn) {
-    if(!_isFunction(fn)) {
-        throw Error(fn+ ' is not a function!');
-    }
-   
-     return function f() {
-        const args = [].slice.call(arguments);
-        if(args.length < fn.length) {
-            return function() {
-                return f.apply(this, args.concat([].slice.call(arguments)))
+export default function _curryN(length, recived, fn) {
+    return function() {
+        var combined = [];
+        var args = [].slice.call(arguments);
+        var argsIdx = 0;
+        var offset = length;
+
+        while(combined.length < recived.length || argsIdx < args.length) {
+            var result;
+            if(combined.length < recived.length && (!_isPlaceholder(recived[combined.length]) || argsIdx >= args.length)) {
+                result = recived[combined.length];
+            } else {
+                result = args[argsIdx];
+                argsIdx++;
             }
-        } else {
-          return  fn.apply(this, args);
+          
+            combined[combined.length] = result;
+            if(!_isPlaceholder(result)) offset -= 1;
+         
         }
-    } 
+        
+        return offset <= 0 ? fn.apply(this, combined) : _arity(offset, _curryN(length, combined, fn));
+    }
 }
+
